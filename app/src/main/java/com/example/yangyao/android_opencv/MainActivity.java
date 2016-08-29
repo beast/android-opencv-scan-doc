747fa5ca6233a3866721afc7e2b97476fcffa2d6
yang yao <yang@livepay.my>
2016-08-29 20:52:09 +0800

    draw lines

--- app/src/main/java/com/example/yangyao/android_opencv/MainActivity.java
+++ app/src/main/java/com/example/yangyao/android_opencv/MainActivity.java
@@ -4,13 +4,17 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     Bitmap srcBitmap;
     Bitmap grayBitmap;
     Bitmap cannyBitmap;
+    Bitmap linesBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,17 +88,19 @@ public class MainActivity extends AppCompatActivity {
     protected void lines(ImageView img) {
         Mat rgbMat = new Mat();
         Mat grayMat = new Mat();
-        Mat cannyMat = new Mat();
+        Mat cannyMat;
+        Mat linesMat = new Mat();
         BitmapFactory.Options o=new BitmapFactory.Options();
 
         // TODO: 29/08/2016  May need to check sample size https://developer.android.com/training/displaying-bitmaps/load-bitmap.html
-        o.inSampleSize = 6;
+        o.inSampleSize = 4;
         o.inDither=false;
 
         // resize
         srcBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.card, o);
         grayBitmap = Bitmap.createBitmap(srcBitmap.getWidth(), srcBitmap.getHeight(), Bitmap.Config.RGB_565);
         cannyBitmap = Bitmap.createBitmap(srcBitmap.getWidth(), srcBitmap.getHeight(), Bitmap.Config.RGB_565);
+        linesBitmap = Bitmap.createBitmap(srcBitmap.getWidth(), srcBitmap.getHeight(), Bitmap.Config.RGB_565);
         Utils.bitmapToMat(srcBitmap, rgbMat);//convert original bitmap to Mat, R G B.
 
         // grayscale
@@ -102,9 +109,33 @@ public class MainActivity extends AppCompatActivity {
         // canny
         cannyMat = getCanny(grayMat);
 
-        // draw lines
+        // HoughLinesP
 
+        int w = srcBitmap.getWidth();
+        int min_w = 200;
+        double scale = Math.min(10.0, w*1.0/ min_w);
+        int w_proc = (int) (w * 1.0 / scale);
+        Imgproc.HoughLinesP(cannyMat,linesMat, 1, Math.PI/180, w_proc/3, w_proc/3, 20 );
 
+        Log.e("opencv","lines.cols" + linesMat.cols());
+        for (int x = 0; x < linesMat.cols(); x++)
+        {
+            double[] vec = linesMat.get(0, x);
+            double x1 = vec[0],
+                    y1 = vec[1],
+                    x2 = vec[2],
+                    y2 = vec[3];
+            Point start = new Point(x1, y1);
+            Point end = new Point(x2, y2);
+
+            Imgproc.line(cannyMat, start, end, new Scalar(255,0,0), 20, Imgproc.LINE_AA, 0);
+
+        }
+
+        Log.e("opencv","completed HoughLines");
+        Log.e("opencv","linesMat size: " + linesMat.size());
+        Log.e("opencv","linesMat size: " + Double.toString(linesMat.size().height) + " x " +Double.toString(linesMat.size().width));
+        Log.e("opencv", "linesBitmap size: " + Integer.toString(linesBitmap.getHeight()) +" x " + Integer.toString(linesBitmap.getWidth()));
         Utils.matToBitmap(cannyMat, cannyBitmap); //convert mat to bitmap
         img.setImageBitmap(cannyBitmap);
     }
@@ -118,4 +149,6 @@ public class MainActivity extends AppCompatActivity {
         Imgproc.Canny(gray, canny, low_threshold, high_threshold);
         return canny;
     }
+
+
 }
